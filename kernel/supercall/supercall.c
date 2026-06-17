bbd07a9aa1b92363125e6b4476ba30190d8c2f3b
xCaptaiN09 <dilsheddilu123@gmail.com>
2026-06-17 09:12:14 +0530

    susfs: delegate v2 command ABI to kernel core

--- kernel/supercall/supercall.c
+++ kernel/supercall/supercall.c
@@ -93,93 +93,6 @@ static int ksu_handle_susfs_prctl(unsigned long cmd, unsigned long arg3,
 	int error = -EOPNOTSUPP;
 
 	switch (cmd) {
-	case CMD_SUSFS_SHOW_VERSION:
-		error = copy_to_user((void __user *)arg3, SUSFS_VERSION,
-				     strlen(SUSFS_VERSION) + 1) ?
-				-EFAULT : 0;
-		break;
-	case CMD_SUSFS_SHOW_ENABLED_FEATURES: {
-		u64 enabled_features = 0;
-#ifdef CONFIG_KSU_SUSFS_SUS_PATH
-		enabled_features |= (1ULL << 0);
-#endif
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-		enabled_features |= (1ULL << 1);
-#endif
-#ifdef CONFIG_KSU_SUSFS_AUTO_ADD_SUS_KSU_DEFAULT_MOUNT
-		enabled_features |= (1ULL << 2);
-#endif
-#ifdef CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT
-		enabled_features |= (1ULL << 3);
-#endif
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-		enabled_features |= (1ULL << 4);
-#endif
-#ifdef CONFIG_KSU_SUSFS_SUS_OVERLAYFS
-		enabled_features |= (1ULL << 5);
-#endif
-#ifdef CONFIG_KSU_SUSFS_TRY_UMOUNT
-		enabled_features |= (1ULL << 6);
-#endif
-#ifdef CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT
-		enabled_features |= (1ULL << 7);
-#endif
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-		enabled_features |= (1ULL << 8);
-#endif
-#ifdef CONFIG_KSU_SUSFS_ENABLE_LOG
-		enabled_features |= (1ULL << 9);
-#endif
-#ifdef CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS
-		enabled_features |= (1ULL << 10);
-#endif
-#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
-		enabled_features |= (1ULL << 11);
-#endif
-#ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-		enabled_features |= (1ULL << 12);
-#endif
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-		enabled_features |= (1ULL << 13);
-#endif
-#ifdef CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT
-		enabled_features |= (1ULL << 14);
-#endif
-		error = copy_to_user((void __user *)arg3, &enabled_features,
-				     sizeof(enabled_features)) ?
-				-EFAULT : 0;
-		break;
-	}
-	case CMD_SUSFS_SHOW_VARIANT:
-		error = copy_to_user((void __user *)arg3, SUSFS_VARIANT,
-				     strlen(SUSFS_VARIANT) + 1) ?
-				-EFAULT : 0;
-		break;
-	case CMD_SUSFS_SHOW_SUS_SU_WORKING_MODE:
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-	{
-		int mode = susfs_get_sus_su_working_mode();
-		error = copy_to_user((void __user *)arg3, &mode, sizeof(mode)) ?
-				-EFAULT : 0;
-		break;
-	}
-#else
-		error = -EOPNOTSUPP;
-		break;
-#endif
-	case CMD_SUSFS_IS_SUS_SU_READY:
-#ifdef CONFIG_KSU_SUSFS_SUS_SU
-	{
-		bool ready = true;
-		error = copy_to_user((void __user *)arg3, &ready,
-				     sizeof(ready)) ?
-				-EFAULT : 0;
-		break;
-	}
-#else
-		error = -EOPNOTSUPP;
-		break;
-#endif
 	case CMD_SUSFS_RUN_UMOUNT_FOR_CURRENT_MNT_NS:
 #ifdef CONFIG_KSU_SUSFS_TRY_UMOUNT
 		susfs_try_umount(current_uid().val);
@@ -201,7 +114,7 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 		     unsigned long arg4, unsigned long arg5)
 {
 #ifdef CONFIG_KSU_SUSFS
-	if (option >= CMD_SUSFS_ADD_SUS_PATH && option <= CMD_SUSFS_SUS_SU) {
+	if (option >= CMD_SUSFS_ADD_SUS_PATH && option <= CMD_SUSFS_ADD_SUS_MAP) {
 		if (current_uid().val != 0 && !is_manager())
 			return -EPERM;
 		return ksu_handle_susfs_prctl(option, arg2, arg4);
@@ -227,7 +140,7 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 #endif
 
 #ifdef CONFIG_KSU_SUSFS
-	if (arg2 >= CMD_SUSFS_ADD_SUS_PATH && arg2 <= CMD_SUSFS_SUS_SU)
+	if (arg2 >= CMD_SUSFS_ADD_SUS_PATH && arg2 <= CMD_SUSFS_ADD_SUS_MAP)
 		return ksu_handle_susfs_prctl(arg2, arg3, arg5);
 #endif
 
