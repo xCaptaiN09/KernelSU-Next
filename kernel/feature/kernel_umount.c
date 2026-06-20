badd1ef7a29b286da430eca851b3cb1e16cf9462
xCaptaiN09 <dilsheddilu123@gmail.com>
2026-06-21 00:54:11 +0530

    fix: add missing #endif for umount_tw_func block

--- kernel/feature/kernel_umount.c
+++ kernel/feature/kernel_umount.c
@@ -105,7 +105,6 @@ void try_umount(const char *mnt, int flags)
 
 struct umount_tw {
 	struct callback_head cb;
-};
 
 #if !defined(CONFIG_KSU_SUSFS) || !defined(CONFIG_KSU_SUSFS_TRY_UMOUNT)
 static void umount_tw_func(struct callback_head *cb)
@@ -126,6 +125,7 @@ static void umount_tw_func(struct callback_head *cb)
 	kfree(tw);
 }
 
+#endif
 int vnd_handle_umount(uid_t old_uid, uid_t new_uid)
 {
 	struct umount_tw *tw;
